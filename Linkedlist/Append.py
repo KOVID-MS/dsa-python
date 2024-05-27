903ba7755e07992bc9fc5e8c542a4760ce1e809f
KOVID-MS <kovidmsk@gmail.com>
2024-05-27 15:42:12 +0530

    commit

---
+++ Linkedlist/Append.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Linkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+my_linked_list = Linkedlist(1)
+my_linked_list.append(2)
+
+print(my_linked_list.head.next.value)
---
+++ Linkedlist/PrintList.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Linkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def print_list(self):
+        temp = self.head
+        while temp != None:
+            print(temp.value)
+            temp = temp.next
+    
+
+my_linked_list = Linkedlist(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.print_list()
