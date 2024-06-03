985b5e3763322e69dadb4cde38714bb173915467
KOVID-MS <kovidmsk@gmail.com>
2024-06-03 12:59:09 +0530

    Update PopFirst.py

--- Linkedlist/PopFirst.py
+++ Linkedlist/PopFirst.py
@@ -28,30 +28,6 @@ class LinkedList:
             self.tail.next = new_Node
             self.tail = self.tail.next
         self.length += 1
-    
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        pre = self.head
-        while temp.next != None:
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return temp
-
-    def prepend(self,value):
-        new_Node = Node(value)
-        if self.length == 0:
-            self.head = self.tail = new_Node
-        new_Node.next = self.head
-        self.head = new_Node
-        self.length +=1
 
     def popfirst(self):
         if self.length == 0:
