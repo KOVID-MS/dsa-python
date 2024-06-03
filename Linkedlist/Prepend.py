3a51f2ba55b2ad14417f50b2e50f218a13a66d06
KOVID-MS <kovidmsk@gmail.com>
2024-06-03 13:00:19 +0530

    Update Prepend.py

--- Linkedlist/Prepend.py
+++ Linkedlist/Prepend.py
@@ -28,22 +28,6 @@ class LinkedList:
             self.tail.next = new_Node
             self.tail = self.tail.next
         self.length += 1
-    
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        pre = self.head
-        while temp.next != None:
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return temp
 
     def prepend(self,value):
         new_Node = Node(value)
@@ -56,8 +40,8 @@ class LinkedList:
 print("Linked List before prepending")
 my_linked_list = LinkedList(1)
 my_linked_list.append(2)
-my_linked_list.pop()
 my_linked_list.print_list()
+
 print("\nLinked List after prepending")
 my_linked_list.prepend(0)
 my_linked_list.print_list()
