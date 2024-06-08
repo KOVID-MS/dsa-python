d8e4cd219cdc9892240059c909c1c73060b75fe0
KOVID-MS <kovidmsk@gmail.com>
2024-06-08 22:54:26 +0530

    Create Remove.py

---
+++ Linkedlist/Remove.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+    
+class Linkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def append(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def get(self,index):
+        if index < 0 or index > self.length:
+            return False
+        
