8afb5156a7ef87731f49934601c3857e161675ca
KOVID-MS <kovidmsk@gmail.com>
2024-06-08 22:33:14 +0530

    Create Set.py

---
+++ Linkedlist/Set.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class Linkedlist:
+    def __init__(self,value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1
+
+    def get(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp
+        
+    def set(self,index,value):
+        node = self.get(index)
+        if (node):
+            node.value = value
+            return True
+
+my_linked_list = Linkedlist(0)
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.set(1,4)
+
+print(my_linked_list.get(1).value)
+
+
+        
+
