1a02f067ea2a5a597215a6ef4596603198f57712
KOVID-MS <kovidmsk@gmail.com>
2024-06-12 22:51:23 +0530

    commit

---
+++ Leetcode exercises/Linkedlist/remove_duplicates.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+    
+    def print_list(self):
+        if self.head is None:
+            print("empty list")
+        else:
+            temp = self.head
+            values = []
+            while temp is not None:
+                values.append(str(temp.value))
+                temp = temp.next
+            print(" -> ".join(values))
+
+
+    def remove_duplicates(self):
+        temp = self.head
+        Set1 = set()
+        pre = self.head        
+        while temp != None:
+            if temp.value not in Set1:
+                Set1.add(temp.value)
+                pre = temp
+            else:
+                pre.next = temp.next
+            temp = temp.next
+        
