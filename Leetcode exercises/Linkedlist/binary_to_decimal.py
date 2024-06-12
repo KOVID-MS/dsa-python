86106542fffabb4d8cbdcbf973c9146fcb2cc7e6
KOVID-MS <kovidmsk@gmail.com>
2024-06-12 23:04:20 +0530

    Update binary_to_decimal.py

---
+++ Leetcode exercises/Linkedlist/binary_to_decimal.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+    
+    def print_list(self):
+        if self.head is None:
+            print("empty list")
+        else:
+            temp = self.head
+            values = []
+            while temp is not None:
+                values.append(str(temp.value))
+                temp = temp.next
+            print(" -> ".join(values)) 
+
+    def binary_to_decimal(self):
+        temp = self.head
+        binary = 0
+        for i in range(self.length-1,-1,-1):
+            binary += temp.value*(pow(2,i))
+            temp = temp.next
+        return binary
