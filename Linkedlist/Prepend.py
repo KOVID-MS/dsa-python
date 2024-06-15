a692c8b2ec03b0fe3f66d9d8cf71895f21088610
KOVID-MS <kovidmsk@gmail.com>
2024-06-15 21:47:18 +0530

    Update Prepend.py

--- Linkedlist/Prepend.py
+++ Linkedlist/Prepend.py
@@ -36,12 +36,3 @@ class LinkedList:
         new_Node.next = self.head
         self.head = new_Node
         self.length +=1
-
-print("Linked List before prepending")
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.print_list()
-
-print("\nLinked List after prepending")
-my_linked_list.prepend(0)
-my_linked_list.print_list()
