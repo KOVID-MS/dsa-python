5d3ab8fc62f18b1dc6e0e59bf6c1d3e11d6080ef
KOVID-MS <kovidmsk@gmail.com>
2024-06-15 21:45:55 +0530

    Update Constructor.py

--- Linkedlist/Constructor.py
+++ Linkedlist/Constructor.py
@@ -9,9 +9,3 @@ class Linkedlist:
         self.head = new_node
         self.tail = new_node
         self.length = 1
-
-my_linked_list = Linkedlist(4)
-
-print("Head value : ", my_linked_list.head.value)
-print("Tail value : ", my_linked_list.tail.value)
-print("Length : ", my_linked_list.length)
