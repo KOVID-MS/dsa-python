0bd6e6575ea1e9308870e9494f0c6379520309b7
KOVID-MS <kovidmsk@gmail.com>
2024-06-15 21:44:04 +0530

    Update find kth node from end.py

--- Leetcode exercises/Linkedlist/find kth node from end.py
+++ Leetcode exercises/Linkedlist/find kth node from end.py
@@ -38,18 +38,6 @@ def find_kth_from_end(self,k):
             fast = fast.next
         return slow
             
-            
-my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
-my_linked_list.append(4)
-my_linked_list.append(5)
-
-
-k = 2
-result = find_kth_from_end(my_linked_list, k)
-
-print(result.value)
 
 
 
