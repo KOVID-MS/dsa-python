e062e58ff08695eeb297b3d59cb4b42523e2e829
KOVID-MS <kovidmsk@gmail.com>
2024-06-17 22:01:47 +0530

    Update Get.py

--- Linkedlist/Get.py
+++ Linkedlist/Get.py
@@ -10,15 +10,6 @@ class Linkedlist:
         self.tail = new_node
         self.length = 1
 
-    def append(self,value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length +=1
 
     def get(self,index):
         if index < 0 or index >= self.length:
