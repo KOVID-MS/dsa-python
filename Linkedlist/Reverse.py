82f89a2a3084dbc03cf18529b8338cdb7ce0f78f
KOVID-MS <kovidmsk@gmail.com>
2024-06-17 22:03:26 +0530

    Update Reverse.py

--- Linkedlist/Reverse.py
+++ Linkedlist/Reverse.py
@@ -10,22 +10,6 @@ class Linkedlist:
         self.tail = new_node
         self.length = 1
 
-    def append(self,value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length +=1
-
-    def print_list(self):
-        temp = self.head
-        while temp != None:
-            print(temp.value)
-            temp = temp.next
-
     def reverse(self):
         if self.length == 0:
             return False
