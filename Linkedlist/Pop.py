4243e3058339a0a1a8bdadbd3c06b027a6f40427
KOVID-MS <kovidmsk@gmail.com>
2024-06-17 22:02:01 +0530

    Update Pop.py

--- Linkedlist/Pop.py
+++ Linkedlist/Pop.py
@@ -10,25 +10,6 @@ class Linkedlist:
         self.tail = new_node
         self.length = 1
 
-    def print_list(self):
-        if self.length == 0:
-            print(None)
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
-        
-
-    def append(self,value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length+=1
-
     def pop(self):
         if self.length == 0:
             return None
