3c9fd11cb9079ae8f33ffba15cf8ef0cb1f233f4
KOVID-MS <kovidmsk@gmail.com>
2024-06-18 22:06:29 +0530

    Update Remove.py

--- Doubly Linkedlist/Remove.py
+++ Doubly Linkedlist/Remove.py
@@ -125,29 +125,5 @@ class DoublyLinkedList:
             self.length -=1
             return temp
 
-# Testing the code
 
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
-my_doubly_linked_list.append(3)
-my_doubly_linked_list.append(4)
-my_doubly_linked_list.append(5)
-
-print('DLL before remove():')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(2).value)
-print('DLL after remove() in middle:')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(0).value)
-print('DLL after remove() of first node:')
-my_doubly_linked_list.print_list()
-
-print('\nRemoved node:')
-print(my_doubly_linked_list.remove(2).value)
-print('DLL after remove() of last node:')
-my_doubly_linked_list.print_list()
 
