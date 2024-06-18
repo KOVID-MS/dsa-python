1f50d4965706200d2e84b2494d3de5363a8c672d
KOVID-MS <kovidmsk@gmail.com>
2024-06-18 22:12:21 +0530

    Update swap_first_last.py

--- Leetcode exercises/Doubly Linkedlist/swap_first_last.py
+++ Leetcode exercises/Doubly Linkedlist/swap_first_last.py
@@ -38,38 +38,3 @@ class DoublyLinkedList:
         self.tail.value = temp
 
 
-my_doubly_linked_list = DoublyLinkedList(1)
-my_doubly_linked_list.append(2)
-my_doubly_linked_list.append(3)
-my_doubly_linked_list.append(4)
-
-
-print('DLL before swap_first_last():')
-my_doubly_linked_list.print_list()
-
-
-my_doubly_linked_list.swap_first_last()
-
-
-print('\nDLL after swap_first_last():')
-my_doubly_linked_list.print_list()
-
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    DLL before swap_first_last():
-    1
-    2
-    3
-    4
-
-    DLL after swap_first_last():
-    4
-    2
-    3
-    1
-
-"""
-
