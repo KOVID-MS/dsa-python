c4f834a42fb2b410b7695cf6cd6a82e821a0151e
KOVID-MS <kovidmsk@gmail.com>
2024-06-18 22:00:47 +0530

    Update Insert.py

--- Doubly Linkedlist/Insert.py
+++ Doubly Linkedlist/Insert.py
@@ -11,12 +11,6 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
-
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -28,20 +22,6 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
-
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.tail
-        if self.length == 1:
-            self.head = None
-            self.tail = None
-        else:
-            self.tail = self.tail.prev
-            self.tail.next = None
-            temp.prev = None
-        self.length -= 1
-        return temp
 
     def prepend(self, value):
         new_node = Node(value)
@@ -55,20 +35,6 @@ class DoublyLinkedList:
         self.length += 1
         return True
 
-    def pop_first(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        if self.length == 1:
-            self.head = None
-            self.tail = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-            temp.next = None
-        self.length -= 1
-        return temp
-
     def get(self, index):
         if index < 0 or index >= self.length:
             return None
@@ -81,13 +47,6 @@ class DoublyLinkedList:
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev
         return temp
-
-    def set_value(self, index, value):
-        temp = self.get(index)
-        if temp:
-            temp.value = value
-            return True
-        return False
 
     def insert(self,index,value):
         if index < 0 or index > self.length:
