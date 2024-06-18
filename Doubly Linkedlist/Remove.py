61a8be24c27d2e26c75076dbc903e0aaf2d34fda
KOVID-MS <kovidmsk@gmail.com>
2024-06-18 22:07:19 +0530

    Update Remove.py

--- Doubly Linkedlist/Remove.py
+++ Doubly Linkedlist/Remove.py
@@ -10,25 +10,7 @@ class DoublyLinkedList:
         self.head = new_node
         self.tail = new_node
         self.length = 1
-
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        self.length += 1
-        return True
-
+        
     def pop(self):
         if self.length == 0:
             return None
@@ -42,18 +24,6 @@ class DoublyLinkedList:
             temp.prev = None
         self.length -= 1
         return temp
-
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.length == 0:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-        self.length += 1
-        return True
 
     def pop_first(self):
         if self.length == 0:
@@ -81,33 +51,6 @@ class DoublyLinkedList:
             for _ in range(self.length - 1, index, -1):
                 temp = temp.prev
         return temp
-
-    def set_value(self, index, value):
-        temp = self.get(index)
-        if temp:
-            temp.value = value
-            return True
-        return False
-
-    def insert(self, index, value):
-        if index < 0 or index > self.length:
-            return False
-        if index == 0:
-            return self.prepend(value)
-        if index == self.length:
-            return self.append(value)
-
-        new_node = Node(value)
-        before = self.get(index - 1)
-        after = before.next
-
-        new_node.prev = before
-        new_node.next = after
-        before.next = new_node
-        after.prev = new_node
-
-        self.length += 1
-        return True
 
     def remove(self,index):
         if index < 0 or index >= self.length:
