6985fec9412d7ff0849b92f90f6b3a729e3af0a5
KOVID-MS <kovidmsk@gmail.com>
2024-06-18 22:12:41 +0530

    Update swap_first_last.py

--- Leetcode exercises/Doubly Linkedlist/swap_first_last.py
+++ Leetcode exercises/Doubly Linkedlist/swap_first_last.py
@@ -12,24 +12,6 @@ class DoublyLinkedList:
         self.tail = new_node
         self.length = 1
 
-    def print_list(self):
-        temp = self.head
-        while temp is not None:
-            print(temp.value)
-            temp = temp.next
-        
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            new_node.prev = self.tail
-            self.tail = new_node
-        self.length += 1
-        return True
-
     def swap_first_last(self):
         if self.length == 0:
             return None
