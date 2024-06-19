5541823471ef9542667c4a667859099c873e0961
KOVID-MS <kovidmsk@gmail.com>
2024-06-19 22:59:54 +0530

    Create swap_pairs.py

---
+++ Leetcode exercises/Doubly Linkedlist/swap_pairs.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def print_list(self):
+        output = []
+        current_node = self.head
+        while current_node is not None:
+            output.append(str(current_node.value))
+            current_node = current_node.next
+        print(" <-> ".join(output))
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = new_node
+            new_node.prev = temp
+        self.length += 1
+        return True
+
+    def swap_pairs(self):
+        if self.head == None:
+            return None
+        if self.head.next == None:
+            return self.head
+        else:
+            temp = self.head
+            self.head = temp.next
+            while temp!= None and temp.next!= None:
+                after = temp.next
+                temp.next = after.next
+                if after.next != None:
+                    after.next.prev = temp
+                after.prev = temp.prev
+                if temp.prev != None:
+                    temp.prev.next = after
+                after.next = temp
+                temp.prev = after
+                
+                temp = temp.next
+        return self.head
+
+
+my_dll = DoublyLinkedList(1)
+my_dll.append(2)
+my_dll.append(3)
+my_dll.append(4)
+
+print('my_dll before swap_pairs:')
+my_dll.print_list()
+
+my_dll.swap_pairs() 
+
+
+print('my_dll after swap_pairs:')
+my_dll.print_list()
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    my_dll before swap_pairs:
+    1 <-> 2 <-> 3 <-> 4
+    ------------------------
+    my_dll after swap_pairs:
+    2 <-> 1 <-> 4 <-> 3
+
+"""
