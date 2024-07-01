b347da335f6d35f076c3c66b40ae13a50baa2ce0
KOVID-MS <kovidmsk@gmail.com>
2024-07-01 15:32:20 +0530

    Update Constructor.py

--- Queue/Constructor.py
+++ Queue/Constructor.py
@@ -9,21 +9,9 @@ class Queue:
         self.first = new_node
         self.last = new_node
         self.length = 1
-
-
-
+        
 my_queue = Queue(4)
 
 print('First:', my_queue.first.value)
 print('Last:', my_queue.last.value)
 print('Length:', my_queue.length)
-
-
-"""
-    EXPECTED OUTPUT:
-    ----------------
-    First: 4
-    Last: 4
-    Length: 1
-
-"""
